12dd0420c8146f35ac7ce697b752386fe2527dda
FN56 <117398168+FN56@users.noreply.github.com>
2026-03-30 10:02:26 +0200

    Add angebote.js with promotional offers

---
+++ script/angebot.js
@@ -0,0 +1,15 @@
+// angebote.js
+const angebote = [
+  {
+    titel: "Frisches Obst",
+    text: "Jetzt Äpfel und Birnen 20% günstiger!"
+  },
+  {
+    titel: "Fleisch-Special",
+    text: "Rindersteaks im Angebot – nur diese Woche!"
+  },
+  {
+    titel: "Gemüse-Deals",
+    text: "Karotten & Tomaten frisch vom Feld"
+  }
+];
